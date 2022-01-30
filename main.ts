f0dbb377de5a96aa1336239123317929faaa3f71
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2022-01-30 14:56:15 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+//%block="Pathfind" blockId="pathFind" color=#2da60b icon="\uf002"
+namespace Pathfind {
+    //%block="Set %sprite follow tile %tile" blockId="followTile" color=#2da60b icon="\uf002"
+    export function followTile(sprite: Sprite, tile: tiles.Tile) {
+        const tilepos = {x: tile.x, y: tile.y}
+        const spritepos = {x: sprite.x, y: sprite.y}
+    }
+}
