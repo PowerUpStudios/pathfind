8795f8b1f512c9d42beb26effa2486bd8fcae195
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2022-01-30 14:58:13 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //%block="Pathfind" blockId="pathFind" color=#2da60b icon="\uf002"
 namespace Pathfind {
     //%block="Set %sprite follow tile %tile" blockId="followTile" color=#2da60b icon="\uf002"
-    export function followTile(sprite: Sprite, tile: tiles.Tile) {
+    export function followTile(sprite: Sprite, tile: tiles.Location) {
         const tilepos = {x: tile.x, y: tile.y}
         const spritepos = {x: sprite.x, y: sprite.y}
     }
